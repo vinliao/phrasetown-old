fdf8ad06c7d4e20fd475ae4bf5bb1a841df4a8a6
Vincent Liao <vincent.liao@protonmail.com>
2022-12-13 13:36:26 +0700

    shorten welcome message

--- src/routes/welcome/+page.svelte
+++ src/routes/welcome/+page.svelte
@@ -3,21 +3,16 @@
 	import { fade } from 'svelte/transition';
 	import { goto } from '$app/navigation';
 
-	new Promise((r) => setTimeout(r, 5000)).then(() => {
+	new Promise((r) => setTimeout(r, 2000)).then(() => {
 		goto('/');
 	});
 </script>
 
 <div in:fade={{ duration: 200 }}>
-	<PageHeader name="About" />
-	<div class="p-4 flex flex-col space-y-2">
-		<p class="font-[900]">Your wallet has been successfully connected. Welcome to Phrasetown!</p>
+	<PageHeader name="Welcome!" />
+	<p class="font-[900]">Connection successful. Welcome to Phrasetown!</p>
 
-		<p>
-			Complaints, comments, feature requests? Ping me on Farcaster @pixel or email me
-			vincent@pixelhack.xyz.
-		</p>
+	<p>You will be redirected to the home page shortly.</p>
 
-		<p>You will be redirected to the home page shortly.</p>
-	</div>
+	<p>🟪⛩️</p>
 </div>
