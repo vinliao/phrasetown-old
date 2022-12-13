74399044286ac1462449987e9c238a094e3acf81
Vincent Liao <vincent.liao@protonmail.com>
2022-12-13 12:21:36 +0700

    notification fetch bug magically fixes itself, add cosmetic changes to mention

--- src/routes/mentions/+page.svelte
+++ src/routes/mentions/+page.svelte
@@ -7,15 +7,10 @@
 
 	let casts: CastInterface[] = [];
 	let endpoints: EndpointInterface[] = [];
-  
-  /**
-   * bug: only this page errors out when trying to fetch from backend
-   * this is just a temporary, ugly workaround
-  */
+
 	$: if ($userHubKeyWritable) {
 		(async () => {
-			const url = import.meta.env.PROD ? 'https://phrasetown.com' : 'http://localhost:5173';
-			const response = await fetch(`${url}/api/fetch-mentions`, {
+			const response = await fetch(`/api/fetch-mentions`, {
 				method: 'PUT',
 				body: JSON.stringify({
 					userHubKey: $userHubKeyWritable
@@ -28,9 +23,26 @@
 	}
 </script>
 
-<div in:fade={{ duration: 200 }}>
+<div>
 	<PageHeader name="Mentions" />
-	{#each casts as cast}
-		<Cast {cast} />
-	{/each}
+	{#if casts.length > 0}
+		<div in:fade={{ duration: 200 }}>
+			{#each casts as cast}
+				<Cast {cast} />
+			{/each}
+		</div>
+	{:else}
+		<div class="flex justify-center py-5">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-6 h-6 animate-spin"
+			>
+				<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+			</svg>
+		</div>
+	{/if}
 </div>
