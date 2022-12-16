c548a489aa6d82d567be708152069a7eb95bbd4f
Vincent Liao <vincent.liao@protonmail.com>
2022-12-16 11:04:04 +0700

    add basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<div
+	class="h-screen w-screen bg-neutral-900 absolute left-0 top-0 flex items-center justify-center"
+>
+	<div class="flex flex-col space-y-6 items-center p-2">
+		<span class="text-base md:text-lg font-[900] text-center"
+			>Oops, something is wrong. Can you try refreshing? Or, go back <a href="/" class="underline"
+				>home</a
+			>.</span
+		>
+		<img src="https://i.imgur.com/lXSWGjR.png" alt="error message" class="h-40 w-40" />
+	</div>
+</div>
